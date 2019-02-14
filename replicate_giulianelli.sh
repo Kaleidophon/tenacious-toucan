391dc144593555f9e020488bd1911c57615ef548
Kaleidophon <Dennis.Ulmer@gmx.de>
2019-02-14 10:24:41 +0100

    :bug: Adding creating of missing paths to replication script

--- replicate_giulianelli.sh
+++ replicate_giulianelli.sh
@@ -1,5 +1,9 @@
+mkdir -p data/corpora/labeled/
 python3 convert_corpora.py
 wget -0 ./data/models/gulordava/model.pt https://dl.fbaipublicfiles.com/colorless-green-rnns/best-models/English/hidden650_batch128_dropout0.2_lr20.0.pt
+mkdir -p data/activations/giulianelli/
+mkdir -p data/activations/gulordava/
+mkdir -p data/classifiers/giulianelli/
 python3 extract.py -c config/extract_gulordava.json
 python3 extract.py -c config/extract_giulianelli.json
 python3 classify.py -c config/classify.json
