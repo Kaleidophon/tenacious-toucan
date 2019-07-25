e8daf3ab5e319d23e777b513e0f51f6774540ab1
Kaleidophon <Dennis.Ulmer@gmx.de>
2019-07-25 12:12:22 +0200

    :sparkles: Finish qualitative analysis code
    * Mark time steps where recoding happened
    * Perform recoding on all, none or select time steps
    * There still might be a bug? Some scores are not the same even using the same model

--- src/analysis/error_signals.py
+++ src/analysis/error_signals.py
@@ -7,23 +7,25 @@ or multiple time steps.
 from argparse import ArgumentParser
 from collections import namedtuple, defaultdict
 import sys
-from typing import List, Tuple
+from typing import List, Tuple, Union, Any
 import random
 
 # EXT
 from diagnnose.config.setup import ConfigSetup
 import matplotlib.pyplot as plt
 import torch
+from torch import Tensor
 from matplotlib.backends.backend_pdf import PdfPages
 import numpy as np
 
 # PROJECT
 from src.models.language_model import LSTMLanguageModel
+from src.recoding.step import AbstractStepPredictor
 from src.models.recoding_lm import RecodingLanguageModel
 from src.utils.corpora import load_data, Corpus
 from src.utils.log import StatsCollector
 from src.utils.test import load_test_set
-from src.utils.types import Device
+from src.utils.types import Device, StepSize
 from src.utils.compatability import RNNCompatabilityMixin as CompatibleRNN
 
 # CONSTANTS
@@ -38,6 +40,7 @@ LATEX_LABELS = {
 
 # TYPES
 ScoredSentence = namedtuple("ScoredSentence", ["sentence", "first_scores", "second_scores"])
+RecodingSteps = Union[List[int], str]
 
 
 def main() -> None:
@@ -52,11 +55,32 @@ def main() -> None:
     pdf_path = config_dict["optional"]["pdf_path"]
     stop_after = config_dict["optional"]["stop_after"]
     num_plots = config_dict["optional"]["num_plots"]
+    first_recoding_steps = config_dict["optional"].get("first_recoding_steps", [])
+    second_recoding_steps = config_dict["optional"].get("second_recoding_steps", [])
+
+    assert type(first_recoding_steps) == list or first_recoding_steps == ["never"], \
+        f"Invalid recoding time steps specified, must be either empty list of ints or 'never', " \
+        f"{str(first_recoding_steps)} found "
+    assert type(second_recoding_steps) == list or second_recoding_steps == ["never"], \
+        f"Invalid recoding time steps specified, must be either empty list of ints or 'never', " \
+        f"{str(second_recoding_steps)} found "
+
+    # Awkward conversion of input args
+    # Convert ["never"] to single string and otherwise the number in the list to str
+    if first_recoding_steps[0] != "never":
+        first_recoding_steps = list(map(int, first_recoding_steps))
+    else:
+        first_recoding_steps = first_recoding_steps[0]
+
+    if second_recoding_steps[0] != "never":
+        second_recoding_steps = list(map(int, second_recoding_steps))
+    else:
+        second_recoding_steps = second_recoding_steps[0]
 
     train_set, _ = load_data(corpus_dir, max_seq_len)
     vocab = defaultdict(lambda: "UNK", {idx: word for word, idx in train_set.vocab.items()})
     vocab[train_set.vocab.unk_idx] = "UNK"
-    test_set = load_test_set(corpus_dir, max_seq_len, train_set.vocab, stop_after)
+    test_set = load_test_set(corpus_dir, max_seq_len, train_set.vocab, stop_after=stop_after)
     del train_set
 
     # Load the model whose weights will be used - if these models have a recoder, it will be removed
@@ -64,9 +88,12 @@ def main() -> None:
     second_models = (torch.load(path, map_location=device) for path in second_model_paths)
 
     # Collect data
-    # TODO: Also enable optional argument of where recoding should take place
-    first_models_data = [extract_data(model, test_set, device, collectables) for model in first_models]
-    second_models_data = [extract_data(model, test_set, device, collectables) for model in second_models]
+    first_models_data = [
+        extract_data(model, test_set, device, first_recoding_steps, collectables) for model in first_models
+    ]
+    second_models_data = [
+        extract_data(model, test_set, device, second_recoding_steps, collectables) for model in second_models
+    ]
 
     # Merge data
     scored_sentences = create_scored_sentences(first_models_data, second_models_data, test_set, vocab)
@@ -75,7 +102,7 @@ def main() -> None:
     pdf = PdfPages(pdf_path) if pdf_path is not None else None
     scored_sentences = scored_sentences if num_plots is None else random.sample(scored_sentences, num_plots)
     for scored_sentence in scored_sentences:
-        plot_perplexities(scored_sentence, model_names, pdf)
+        plot_scores(scored_sentence, model_names, first_recoding_steps, second_recoding_steps, pdf)
 
     pdf.close()
 
@@ -108,7 +135,7 @@ def create_scored_sentences(first_models_data: List[List[dict]], second_models_d
     return scored_sentences
 
 
-def extract_data(model: LSTMLanguageModel, test_set: Corpus, device: Device,
+def extract_data(model: LSTMLanguageModel, test_set: Corpus, device: Device, recoding_steps: RecodingSteps,
                  collectables: List[str] = ("out", "out_prime", "delta", "delta_prime")) -> List[dict]:
     """
     Collect the perplexities of a model for all the words in a corpus.
@@ -121,10 +148,12 @@ def extract_data(model: LSTMLanguageModel, test_set: Corpus, device: Device,
         Test corpus the model is being tested on.
     device: Device
         The device the evaluation is being performed on.
+    recoding_steps: RecodingSteps
+        Define the time steps for which recoding should take place. If none are specified, recoding takes place at
+        every time step.
     collectables: List[str]
         Define what data should be collected.
     """
-    # TODO: Add functionality to only recode at certain time steps?
     def perplexity(tensor: torch.Tensor) -> torch.Tensor:
         return - torch.log2(tensor)  # 2 ^ (-x log2 x) = 2 ^ (log2 x ^ -x) = x ^ -x
 
@@ -136,6 +165,15 @@ def extract_data(model: LSTMLanguageModel, test_set: Corpus, device: Device,
     model.diagnostics = True  # Return both the original and redecoded output distribution
 
     # Create dummy predictors
+    dummy_predictors = {
+        l: [DummyPredictor().to(device), DummyPredictor().to(device)]
+        for l in range(model.num_layers)
+    }
+
+    # Don't recode when it is explicity specified to NEVER recode or if specific steps are specified which the current
+    # step doesn't respond to
+    dont_recode = lambda t: (recoding_steps == "never" or (t not in recoding_steps and len(recoding_steps) > 0))
+
     for sentence in test_set:
         sentence_data = defaultdict(list)
 
@@ -143,9 +181,18 @@ def extract_data(model: LSTMLanguageModel, test_set: Corpus, device: Device,
             input_vars = sentence[t].unsqueeze(0).to(device)
 
             if isinstance(model, RecodingLanguageModel):
+                # If no recoding is supposed to take place, switch step size predictors with ones always giving zeros
+                if dont_recode(t):
+                    model_predictors, model.mechanism.predictors = model.mechanism.predictors, dummy_predictors
+
                 re_output_dist, output_dist, hidden = model(
                     input_vars, hidden, target_idx=sentence[t + 1].unsqueeze(0).to(device)
                 )
+
+                # Swap back
+                if dont_recode(t):
+                    model.mechanism.predictors = model_predictors
+
             else:
                 output_dist, hidden = model(
                     input_vars, hidden, target_idx=sentence[t + 1].unsqueeze(0).to(device)
@@ -201,7 +248,8 @@ def extract_data(model: LSTMLanguageModel, test_set: Corpus, device: Device,
     return all_sentence_data
 
 
-def plot_perplexities(scored_sentence,  model_names, pdf=None) -> None:
+def plot_scores(scored_sentence, model_names, first_recoding_steps: RecodingSteps, second_recoding_steps: RecodingSteps,
+                pdf=None) -> None:
     """
     Plot the word perplexity scores of two models for the same sentence and potentially add it to a pdf.
 
@@ -211,6 +259,11 @@ def plot_perplexities(scored_sentence,  model_names, pdf=None) -> None:
         Named tuple with data about how multiple runs of the same model behaved w.r.t. to a single sentence.
     model_names: list
         List of length two with the names of the models for the plot legend.
+    first_recoding_steps: RecodingSteps
+        Steps for which recoding takes places for the first model. These time steps will be marked accordingly. If
+        None, recoding takes place at all time steps and no time step is marked for readability.
+    second_recoding_steps: RecodingSteps
+        Same as above but for the second model.
     pdf: Optional[PdfPages]
         PDF the plot is written to if given. Otherwise the plot is shown on screen.
     """
@@ -290,20 +343,41 @@ def plot_perplexities(scored_sentence,  model_names, pdf=None) -> None:
 
     # Draw vertical lines
     for x_ in range(0, len(tokens), 2):
-        plt.axvline(x=x_, alpha=0.8, color="gray", linewidth=0.25)
+        t = x_ / 2
+
+        # Mark recoding for first model
+        if first_recoding_steps != "never" and t in first_recoding_steps:
+            plt.axvline(
+                x=x_, color=PLOT_COLORS["delta"][0], linewidth=1, linestyle="dashed",
+                label=f"Recoding step {model_names[0]}"
+            )
+
+        # Mark recoding for second model
+        elif second_recoding_steps != "never" and t in second_recoding_steps:
+            plt.axvline(
+                x=x_, color=PLOT_COLORS["delta"][1], linewidth=1, linestyle="dashed",
+                label=f"Recoding step {model_names[1]}"
+            )
+
+        # Mark non-recoding time stop OR if all time steps use recoding
+        else:
+            plt.axvline(x=x_, alpha=0.8, color="gray", linewidth=0.25)
 
     # Rotate the tick labels and set their alignment.
     plt.setp(ax1.get_xticklabels(), rotation=45, ha="right",
              rotation_mode="anchor")
 
-    plt.tight_layout()
-
     if "out" in data_keys:
         handles1, labels1 = ax1.get_legend_handles_labels()
         handels2, labels2 = ax2.get_legend_handles_labels()
-        plt.legend(handles1 + handels2, labels1 + labels2, loc="upper left")
+        plt.legend(
+            handles1 + handels2, labels1 + labels2,
+            loc='upper center', bbox_to_anchor=(0.5, -0.175), fontsize=8, fancybox=True, ncol=4
+        )
     else:
-        plt.legend(loc="upper left")
+        plt.legend(fontsize=8, loc='upper center', bbox_to_anchor=(0.5, -0.175), fancybox=True, ncol=4)
+
+    plt.tight_layout()
 
     if pdf:
         pdf.savefig(fig)
@@ -313,6 +387,32 @@ def plot_perplexities(scored_sentence,  model_names, pdf=None) -> None:
     plt.close()
 
 
+class DummyPredictor(AbstractStepPredictor):
+    """
+    Dummy predictor which always predicts a step size of zero. Useful when you want to avoid recoding to take place.
+    """
+
+    def forward(self, hidden: Tensor, out: Tensor, device: torch.device, **additional: Any) -> StepSize:
+        """
+        Prediction step.
+
+        Parameters
+        ----------
+        hidden: Tensor
+            Current hidden state used to determine step size.
+        out: Tensor
+            Output Tensor of current time step.
+        device: torch.device
+            Torch device the model is being trained on (e.g. "cpu" or "cuda").
+
+        Returns
+        -------
+        step_size: StepSize
+            Batch size x 1 tensor of predicted step sizes per batch instance or one single float for the whole batch.
+        """
+        return torch.Tensor([0]).to(device)
+
+
 def manage_config() -> dict:
     """
     Parse a config file (if given), overwrite with command line arguments and return everything as dictionary
@@ -320,7 +420,10 @@ def manage_config() -> dict:
     """
     required_args = {"corpus_dir", "max_seq_len", "first", "second", "device", "collectables",
                      "model_names"}
-    arg_groups = {"general": required_args, "optional": {"stop_after", "pdf_path", "num_plots"}}
+    arg_groups = {
+        "general": required_args,
+        "optional": {"stop_after", "pdf_path", "num_plots", "first_recoding_steps", "second_recoding_steps"}
+    }
     argparser = init_argparser()
     config_object = ConfigSetup(argparser, required_args, arg_groups)
     config_dict = config_object.config_dict
@@ -351,6 +454,13 @@ def init_argparser() -> ArgumentParser:
                           help="Information that should be plotted.\nTarget word probability (out)\nTarget word "
                                "probability after recoding (out_prime)\nRecoding error signal (delta)\nError signal "
                                "after recoding (delta_prime)", default=["out", "out_prime", "delta", "delta_prime"])
+    from_cmd.add_argument("--first_recoding_steps", nargs="+", default=[],
+                          help="Explicitly define the time steps when recoding is supposed to take place for the first"
+                               "model. If none are specified, recoding takes place at all time steps. If 'never' is "
+                               "specified, it is used at no time step.")
+    from_cmd.add_argument("--second_recoding_steps", nargs="+", default=[],
+                          help="Explicitly define the time steps when recoding is supposed to take place for the second"
+                               "model. If none are specified, recoding takes place at all time steps.")
 
     # Plotting options
     from_cmd.add_argument("--pdf_path", type=str, help="Path to pdf with plots.")
